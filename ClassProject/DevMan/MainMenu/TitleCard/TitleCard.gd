b5be93793ea899aafd9d721ca880cda8883947e3
Craig Moritz <craig.moritz513@gmail.com>
2022-10-09 18:04:36 -0500

    Working splash screen
    Splash Screen to Main Menu to Level Select (placeholder).

---
+++ ClassProject/DevMan/MainMenu/TitleCard/TitleCard.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+const title_color := Color.blueviolet
+    
+func _input(event):
+    print( "input event is:", event)
+    if event.is_action_pressed("ui_cancel"):
+        get_tree().quit( 0 )
+    if event.is_action_pressed("ui_accept"):
+        get_tree().change_scene("res://MainMenu/MainMenu.tscn")
