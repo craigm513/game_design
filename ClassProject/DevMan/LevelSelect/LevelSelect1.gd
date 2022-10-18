cc4f81caa10f0b0cf4435bcb8356d908a4fda669
Craig Moritz <craig.moritz513@gmail.com>
2022-10-17 19:34:55 -0500

    level select screen works

--- ClassProject/DevMan/LevelSelect/LevelSelect1.gd
+++ ClassProject/DevMan/LevelSelect/LevelSelect1.gd
@@ -1,2 +1,21 @@
 extends Node
 
+
+
+func _on_GraveYardButton_pressed():
+    get_tree().change_scene("res://Graveyard1/Graveyard1.tscn")
+
+func _on_ThemeParkButton_pressed():
+    get_tree().change_scene("res://6flags1/6flags1.tscn")
+
+func _on_DeepSeaButton_pressed():
+    get_tree().change_scene("res://DeepSea1/DeepSea1.tscn")
+
+func _on_SpaceButton_pressed():
+    get_tree().change_scene("res://Space1/Space1.tscn")
+
+func _on_WithTimeButton_pressed():
+    get_tree().change_scene("res://WithTime1/WithTime1.tscn")
+
+func _on_OptionsButton_pressed():
+    get_tree().change_scene("res://MainMenu/MainMenu.tscn")
