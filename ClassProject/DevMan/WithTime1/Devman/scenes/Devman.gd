97d8bac12cbce623511522f3b7ab891da032f754
Leonardo Hernandez <ninihernandez@hotmail.com>
2022-10-19 00:43:07 -0500

    Updates on all 3 files

--- ClassProject/DevMan/WithTime1/Devman/scenes/Devman.gd
+++ ClassProject/DevMan/WithTime1/Devman/scenes/Devman.gd
@@ -6,10 +6,19 @@ const GRAVITY = 35
 const JUMPFORCE = -1100
 
 func _physics_process(_delta):
+	
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
-	if Input.is_action_pressed("left"):
+		$Sprite.play("walk")
+	elif Input.is_action_pressed("left"):
 		velocity.x = -SPEED
+		$Sprite.play("walk")
+	else:
+		$Sprite.play("idle")
+		
+	if not is_on_floor():
+		$Sprite.play("air")
+	
 	
 	velocity.y = velocity.y + GRAVITY
 	
