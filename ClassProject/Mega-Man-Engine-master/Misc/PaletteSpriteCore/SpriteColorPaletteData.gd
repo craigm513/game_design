558234c34b244528e5221e31519c8e093db7381e
Craig Moritz <craig.moritz513@gmail.com>
2022-11-05 13:31:34 -0500

    Added MegaMan
    Added a working level of Mega man in GoDot for reference.

---
+++ ClassProject/Mega-Man-Engine-master/Misc/PaletteSpriteCore/SpriteColorPaletteData.gd
@@ -0,0 +1,10 @@
+#Sprite Color Palette Data
+#Code by: First
+
+extends Resource
+
+class_name SpriteColorPaletteData
+
+export (NESColorPalette.NesColor) var primary_color
+export (NESColorPalette.NesColor) var secondary_color
+export (NESColorPalette.NesColor) var outline_color
