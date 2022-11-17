12f2cded522e71022a8ba2d020bc97b591979dfc
JaedonTW <jaedonwaller@gmail.com>
2022-11-16 20:56:32 -0600

    Looping Audio

--- ClassProject/DevMan/Credits/GodotCredits.gd
+++ ClassProject/DevMan/Credits/GodotCredits.gd
@@ -21,111 +21,111 @@ var curr_line := 0
 var lines := []
 
 var credits = [
-    [
-        "A game by Decoders"
-    ],[
-        "Main Character Art",
-        "",
-        "Cail Keeling"        
-    ],[
-        "Theme park level design",
-        "",
-        "Jaedon Waller"
-    ],[
-        "Graveyard level design",
-        "",
-        "Ja\'lun Morris"
-    ],[
-        "With time level design",
-        "",
-        "Leonardo Hernandez"
-    ],[
-        "Character movement and game physics",
-        "",
-        "Leonardo Hernandez", 
-        "Ja\'lun Morris"
-    ],[
-        "Main Menu, Level Select, Victory and Credits",
-        "",
-        "Craig Moritz"
-    ],[
-        "Music and Sfx",
-        "",
-        "Jaedon Waller"
-    ],[
-        "Online resources from",
-        "",
-        "www.spriters-resource.com"
-        
-    ],[
-        "Thanks for playing!"
-    ]
+	[
+		"A game by Decoders"
+	],[
+		"Main Character Art",
+		"",
+		"Cail Keeling"        
+	],[
+		"Theme park level design",
+		"",
+		"Jaedon Waller"
+	],[
+		"Graveyard level design",
+		"",
+		"Ja\'lun Morris"
+	],[
+		"With time level design",
+		"",
+		"Leonardo Hernandez"
+	],[
+		"Character movement and game physics",
+		"",
+		"Leonardo Hernandez", 
+		"Ja\'lun Morris"
+	],[
+		"Main Menu, Level Select, Victory and Credits",
+		"",
+		"Craig Moritz"
+	],[
+		"Music and Sfx",
+		"",
+		"Jaedon Waller"
+	],[
+		"Online resources from",
+		"",
+		"www.spriters-resource.com"
+		
+	],[
+		"Thanks for playing!"
+	]
 ]
 
 
 func _process(delta):
 # warning-ignore:shadowed_variable
-    var scroll_speed = base_speed * delta
-    
-    if section_next:
-        section_timer += delta * speed_up_multiplier if speed_up else delta
-        if section_timer >= section_time:
-            section_timer -= section_time
-            
-            if credits.size() > 0:
-                started = true
-                section = credits.pop_front()
-                curr_line = 0
-                add_line()
-    
-    else:
-        line_timer += delta * speed_up_multiplier if speed_up else delta
-        if line_timer >= line_time:
-            line_timer -= line_time
-            add_line()
-    
-    if speed_up:
-        scroll_speed *= speed_up_multiplier
-    
-    if lines.size() > 0:
-        for l in lines:
-            l.rect_position.y -= scroll_speed
-            if l.rect_position.y < -l.get_line_height():
-                lines.erase(l)
-                l.queue_free()
-    elif started:
-        finish()
+	var scroll_speed = base_speed * delta
+	
+	if section_next:
+		section_timer += delta * speed_up_multiplier if speed_up else delta
+		if section_timer >= section_time:
+			section_timer -= section_time
+			
+			if credits.size() > 0:
+				started = true
+				section = credits.pop_front()
+				curr_line = 0
+				add_line()
+	
+	else:
+		line_timer += delta * speed_up_multiplier if speed_up else delta
+		if line_timer >= line_time:
+			line_timer -= line_time
+			add_line()
+	
+	if speed_up:
+		scroll_speed *= speed_up_multiplier
+	
+	if lines.size() > 0:
+		for l in lines:
+			l.rect_position.y -= scroll_speed
+			if l.rect_position.y < -l.get_line_height():
+				lines.erase(l)
+				l.queue_free()
+	elif started:
+		finish()
 
 
 func finish():
-    if not finished:
-        finished = true
-        # NOTE: This is called when the credits finish
-        # - Hook up your code to return to the relevant scene here, eg...
-        #get_tree().change_scene("res://scenes/MainMenu.tscn")
+	if not finished:
+		finished = true
+		# NOTE: This is called when the credits finish
+		# - Hook up your code to return to the relevant scene here, eg...
+		#get_tree().change_scene("res://scenes/MainMenu.tscn")
 # warning-ignore:return_value_discarded
-        get_tree().change_scene("res://MainMenu/MainMenu.tscn")
+		get_tree().change_scene("res://MainMenu/MainMenu.tscn")
 
 func add_line():
-    var new_line = line.duplicate()
-    new_line.text = section.pop_front()
-    lines.append(new_line)
-    if curr_line == 0:
-        new_line.add_color_override("font_color", title_color)
-    $CreditsContainer.add_child(new_line)
-    
-    if section.size() > 0:
-        curr_line += 1
-        section_next = false
-    else:
-        section_next = true
+	var new_line = line.duplicate()
+	new_line.text = section.pop_front()
+	lines.append(new_line)
+	if curr_line == 0:
+		new_line.add_color_override("font_color", title_color)
+	$CreditsContainer.add_child(new_line)
+	
+	if section.size() > 0:
+		curr_line += 1
+		section_next = false
+	else:
+		section_next = true
 
 
 func _input(event):
-    if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("ui_cancel"):
 # warning-ignore:return_value_discarded
-        get_tree().change_scene("res://MainMenu/MainMenu.tscn")
-    if event.is_action_pressed("ui_down") and !event.is_echo():
-        speed_up = true
-    if event.is_action_released("ui_down") and !event.is_echo():
-        speed_up = false
+		get_tree().change_scene("res://MainMenu/MainMenu.tscn")
+	if event.is_action_pressed("ui_down") and !event.is_echo():
+		speed_up = true
+	if event.is_action_released("ui_down") and !event.is_echo():
+		speed_up = false
--- ClassProject/DevMan/LevelSelect/LevelSelect1.gd
+++ ClassProject/DevMan/LevelSelect/LevelSelect1.gd
@@ -3,29 +3,29 @@ extends Node
 
 
 func _on_GraveYardButton_pressed():
-    get_tree().change_scene("res://Graveyard/Scenes/L1.tscn")
+	get_tree().change_scene("res://Graveyard/Scenes/L1.tscn")
 
 func _on_ThemeParkButton_pressed():
-    get_tree().change_scene("res://ThemePark/Scene/6flags1.tscn")
+	get_tree().change_scene("res://ThemePark/Scene/6flags1.tscn")
 
 func _on_DeepSeaButton_pressed():
-    get_tree().change_scene("res://DeepSea1/DeepSea1.tscn")
+	get_tree().change_scene("res://DeepSea1/DeepSea1.tscn")
 
 func _on_SpaceButton_pressed():
-    # if SpaceButton is pressed and another 
-    get_tree().change_scene("res://Space1/Space1.tscn")
+	# if SpaceButton is pressed and another 
+	get_tree().change_scene("res://Space1/Space1.tscn")
 
 func _on_WithTimeButton_pressed():
-    get_tree().change_scene("res://WithTime1/Devman/scenes/withTime.tscn")
+	get_tree().change_scene("res://WithTime1/Devman/scenes/withTime.tscn")
 
 func _on_BackButton_pressed():
-    get_tree().change_scene("res://MainMenu/MainMenu.tscn")
+	get_tree().change_scene("res://MainMenu/MainMenu.tscn")
 
 func _input(event):
-    if event.is_action_pressed( "ui_cancel" ) :
-        get_tree().change_scene("res://MainMenu/MainMenu.tscn")
-    if event.is_action_pressed( "ui_accept" ) :
-        get_tree().change_scene("res://Space1/reference/Stage.tscn")
-    if event.is_action_pressed( "ui_end" ) :
-        get_tree().change_scene("res://Victory/Victory1.tscn")
-    
+	if event.is_action_pressed( "ui_cancel" ) :
+		get_tree().change_scene("res://MainMenu/MainMenu.tscn")
+	if event.is_action_pressed( "ui_accept" ) :
+		get_tree().change_scene("res://Space1/reference/Stage.tscn")
+	if event.is_action_pressed( "ui_end" ) :
+		get_tree().change_scene("res://Victory/Victory1.tscn")
+	
