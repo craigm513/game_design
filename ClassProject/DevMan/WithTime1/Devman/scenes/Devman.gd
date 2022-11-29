cc364c4d09c19744c3e50e179cb46265331db195
Leonardo Hernandez <ninihernandez@hotmail.com>
2022-11-28 21:08:56 -0600

    Jumping movement fixed, run/acceleration added

--- ClassProject/DevMan/WithTime1/Devman/scenes/Devman.gd
+++ ClassProject/DevMan/WithTime1/Devman/scenes/Devman.gd
@@ -1,47 +1,88 @@
 extends KinematicBody2D
 
+
+enum States {AIR = 1, FLOOR, LADDER, WALL}
+var state = States.AIR
 var velocity = Vector2(0,0)
 var coins = 0
-const SPEED = 180
-const GRAVITY = 35
-const JUMPFORCE = -1100
+const SPEED = 135
+const GRAVITY = 28
+const JUMPFORCE = -900
+const RUNSPEED = 200
 
 var JumpSoundEffect = "res://Audio/Sound Effects/JumpSoundEffect.mp3";
 var FallSoundEffect = "res://Audio/Sound Effects/FallSoundEffect.wav";
 onready var audioStream = get_parent().get_node("Audio/SoundEffect/AudioStreamPlayer");
 
 func _physics_process(_delta):
-	
-	if Input.is_action_pressed("right"):
-		velocity.x = SPEED
-		$Sprite.play("walk")
-		$Sprite.flip_h = false
-	elif Input.is_action_pressed("left"):
-		velocity.x = -SPEED
-		$Sprite.play("walk")
-		$Sprite.flip_h = true
-	else:
-		$Sprite.play("idle")
-		
-	if not is_on_floor():
-		$Sprite.play("air")
-	
+	print(velocity.x)
+	match state:
+		States.AIR:
+			if is_on_floor():
+				state = States.FLOOR
+				continue
+			# the line below loops while devman is in the air, fix it to make it run once
+			$Sprite.play("air")
+			if Input.is_action_pressed("right"):
+				velocity.x = lerp(velocity.x,SPEED,0.1) if velocity.x < SPEED else lerp(velocity.x,SPEED,0.03)
+				$Sprite.flip_h = false
+			elif Input.is_action_pressed("left"):
+				velocity.x = lerp(velocity.x,-SPEED,0.1) if velocity.x < SPEED else lerp(velocity.x,-SPEED,0.03)
+				$Sprite.flip_h = true
+			else:
+				# might add $Sprite.play("idle") to show no movement 
+				#if left or right isnt pressed while in air
+				velocity.x = lerp(velocity.x,0,0.2)
+			move_and_fall()
+		States.FLOOR:
+			if not is_on_floor():
+				state = States.AIR
+			if Input.is_action_pressed("right"):
+				if Input.is_action_pressed("run"):
+					velocity.x = lerp(velocity.x,RUNSPEED,0.1)
+					$Sprite.set_speed_scale(1.5)
+				else:
+					velocity.x = lerp(velocity.x,SPEED,0.1)
+					$Sprite.set_speed_scale(1.0)
+				$Sprite.play("walk")
+				$Sprite.flip_h = false
+			elif Input.is_action_pressed("left"):
+				if Input.is_action_pressed("run"):
+					velocity.x = lerp(velocity.x,-RUNSPEED,0.1)
+					$Sprite.set_speed_scale(1.5)
+				else:
+					velocity.x = lerp(velocity.x,-SPEED,0.1)
+					$Sprite.set_speed_scale(1.0)
+				$Sprite.play("walk")
+				$Sprite.flip_h = true
+			else:
+				$Sprite.play("idle")
+				velocity.x = lerp(velocity.x,0,0.2)
+				
+			if Input.is_action_just_pressed("jump"):
+				velocity.y = JUMPFORCE
+				audioStream.stream = load(JumpSoundEffect);
+				audioStream.play();
+				state = States.AIR
+				
+			move_and_fall()
+		States.LADDER:
+			pass
+		States. WALL:
+			pass
 
-	velocity.y = velocity.y + GRAVITY
+	#velocity.y = velocity.y + GRAVITY
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMPFORCE
-		audioStream.stream = load(JumpSoundEffect);
-		audioStream.play();
-		
-	
-	velocity = move_and_slide(velocity,Vector2.UP)
+	#velocity = move_and_slide(velocity,Vector2.UP)
 
-	velocity.x = lerp(velocity.x,0,0.2)
+	#velocity.x = lerp(velocity.x,0,0.2)
 	
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().change_scene("res://LevelSelect/LevelSelect1.tscn")
 
+func move_and_fall():
+	velocity.y = velocity.y + GRAVITY
+	velocity = move_and_slide(velocity,Vector2.UP)
 
 func _on_fallZone_body_entered(body):
 	audioStream.stream = load(FallSoundEffect);
